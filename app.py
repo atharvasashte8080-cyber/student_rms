e33a4db10ad84061e17e75a702ff9a4248618c9d
omipawar05-cpu <omipawar05@gmail.com>
2026-04-01 10:50:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -403,5 +403,7 @@ def profile():
     return render_template('profile.html', user=user)
 
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    # Render will assign a port automatically; fallback to 5000 locally
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
